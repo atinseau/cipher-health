5dbedc7bfbbf82642c418e16bb966085c978c98d
atinseau <arthurtinseau@live.fr>
2024-01-08 08:41:53 +0100

    syncro

---
+++ apps/frontend/src/components/Inputs/DateInput.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useState } from 'react';
+import DatePicker from 'react-datepicker'
+
+type DateInputProps = {
+
+}
+
+export default function DateInput() {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  return (
+    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+  );
+}
--- k8s/frontend/docker/build.Dockerfile
+++ k8s/frontend/docker/build.Dockerfile
@@ -6,10 +6,7 @@ WORKDIR /app
 
 RUN npm install -g pnpm
 RUN pnpm install --frozen-lockfile
-
-RUN ls -la 
-
 RUN pnpm build --filter=@cipher-health/frontend
 
 
-CMD ["pnpm", "--filter", "@cipher-health/frontend", "start"]
+CMD ["pnpm", "--filter=@cipher-health/frontend", "start"]
