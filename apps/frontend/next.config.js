50ead421443eed85a8bbad7ce2254915158cd4b7
atinseau <arthurtinseau@live.fr>
2024-01-12 18:29:06 +0100

    add Cache-Control on svg

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -7,13 +7,26 @@ const million = require("million/compiler");
 
 const path = require('path')
 
-const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
+// const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: "/(.*).svg",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      }
+    ]
+  },
   webpack: (config) => {
-    config.plugins.push(new DuplicatePackageCheckerPlugin())
+    // config.plugins.push(new DuplicatePackageCheckerPlugin())
 
     config.resolve.alias['tailwind-merge'] = path.resolve(
       __dirname,
