ad330116ad7135c7069254a68a1967695f90832f
atinseau <arthurtinseau@live.fr>
2024-01-22 19:40:17 +0100

    syncro

--- k8s/api/docker/start.sh
+++ k8s/api/docker/start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-RETRIES=5
+RETRIES=20
 
 until pg_isready -h postgres-service > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
+
+if [ $RETRIES -eq 0 ]; then
+  echo "Postgres server never came up, exiting"
+  exit 1
+fi
 
 cd apps/api
 npx prisma migrate deploy
