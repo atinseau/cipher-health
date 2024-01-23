3f4727877458bff367e971a4b8d69bbe4a7277ec
atinseau <arthurtinseau@live.fr>
2024-01-23 18:23:11 +0100

    syncro

--- k8s/api/docker/start.sh
+++ k8s/api/docker/start.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-RETRIES=20
+POSTGRES_RETRIES=20
+REDIS_RETRIES=20
 
-until pg_isready -h postgres-service > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until pg_isready -h postgres-service > /dev/null 2>&1 || [ $POSTGRES_RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
 
-if [ $RETRIES -eq 0 ]; then
+if [ $POSTGRES_RETRIES -eq 0 ]; then
   echo "Postgres server never came up, exiting"
+  exit 1
+fi
+
+until redis-cli -h dragonfly-service ping > /dev/null || [ $REDIS_RETRIES -eq 0 ]; do
+  echo "Waiting for redis server, $((RETRIES--)) remaining attempts..."
+  sleep 1
+done
+
+if [ $REDIS_RETRIES -eq 0 ]; then
+  echo "Redis server never came up, exiting"
   exit 1
 fi
 
 cd apps/api
 npx prisma migrate deploy
 
-# NODE_ENV=production node dist/main
-
-tail -f /dev/null
+NODE_ENV=production node dist/main
